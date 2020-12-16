da2d0b8aa0a9597f59b8bd3c8bcbf26e58edd46f
Matthew Kinzler <matthew.kinzler@gmail.com>
2020-12-16 13:05:19 -0500

    update writer with generalized paths

--- writer.py
+++ writer.py
@@ -1,50 +1,25 @@
 class Writer:
 
     def write_list_mebes_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/listVrfyRunInfoMEBES', 'a') as f:
+        with open(test_List_MEBES, 'a') as f:
             f.write(pptype_values)
 
     def write_next_mebes_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/nextVrfyRunInfoMEBES', 'w') as f:
-            f.write(pptype_values)
-
-    def write_list_cluster_mebes_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/listVrfyRunInfoCLUSTERMEBES', 'a') as f:
-            f.write(pptype_values)
-
-    def write_next_cluster_mebes_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/nextVrfyRunInfoCLUSTERMEBES', 'w') as f:
+        with open(test_next_MEBES, 'w') as f:
             f.write(pptype_values)
 
     def write_list_jeol_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/listVrfyRunInfoJEOL', 'a') as f:
+        with open(test_list_JEOL, 'a') as f:
             f.write(pptype_values)
 
     def write_next_jeol_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/nextVrfyRunInfoJEOL', 'w') as f:
-            f.write(pptype_values)
-
-    def write_list_cluster_jeol_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/listVrfyRunInfoCLUSTERJEOL', 'a') as f:
-            f.write(pptype_values)
-
-    def write_next_cluster_jeol_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/nextVrfyRunInfoCLUSTERJEOL', 'w') as f:
+        with open(test_next_JEOL, 'w') as f:
             f.write(pptype_values)
 
     def write_list_nuflare_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/listVrfyRunInfoNUFLARE', 'a') as f:
+        with open(test_list_NF, 'a') as f:
             f.write(pptype_values)
 
     def write_next_nuflare_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/nextVrfyRunInfoNUFLARE', 'w') as f:
+        with open(test_next_NF, 'w') as f:
             f.write(pptype_values)
-
-    def write_list_cluster_nuflare_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/listVrfyRunInfoCLUSTERNUFLARE', 'a') as f:
-            f.write(pptype_values)
-
-    def write_next_cluster_nuflare_file(self, pptype_values):
-        with open('/apps/VRFY/dsg2nc/config/nextVrfyRunInfoCLUSTERNUFLARE', 'w') as f:
-            f.write(pptype_values)
-
